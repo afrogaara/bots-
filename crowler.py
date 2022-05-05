a2c9c944350dfcaaca889bc3e919186c889b371c
Roberto César <robertocoliver@protonmail.com>
2022-05-05 11:03:55 -0300

    crowler em python ( desenvolvendo

--- crowler.py
+++ crowler.py
@@ -1,6 +1,14 @@
 import requests 
 from bs4 import BeautifulSoup # manipular html 
 
+
+def menu(url):
+    print(f"1) encontrar links do {url}")
+    print(f"2) encontrar class do {url}")
+    print("3) para sair.")
+    pass
+
+
 def buscar_site(url):
     try:
         requisicao = requests.get(url)
@@ -25,13 +33,15 @@ def parsing_html(response_html):
 
 #Encontra o inicio e o fechamento da tag 
 #E da tag vc pega apenas os links 
+
 def find_all_links(html):
     try:
-        links = html.find_all("a")
-        for link in links: 
+        links = html.find_all("a")    
+        for link in links:
             print(link["href"])
     except Exception as error:
-        print("Link não encontrado" ,error)
+        print(error)
+    pass
 
 def find_all_class(html):
     try:
@@ -41,20 +51,41 @@ def find_all_class(html):
     except Exception as error:
         print(error)
 
-def chosse_class(soup):
+def chosse_class(html):
+    links = list() 
     opc = input("Digite um dos class a cima: ")
-    sourcerr = soup.find("div", class_=opc)
-    print(sourcerr)
+    try:
+        m = html.find("div", class_=opc)
+        f = m.find_all("a")
+        for cada in f:
+            link = cada['href']
+            links.append(link)
+        return links 
+    except:
+        print("não encontrado!")
 
 
 url = input("URL: ")
-
 
 
 if __name__== "__main__":
     soup = buscar_site(url)
     if soup:
         html = parsing_html(soup)
-        find_all_class(html)
-        chosse_class(html)        
+    while True:
+        menu(url)
+        opc = input(": ")[0]
+        if opc == '1':
+            find_all_links(html)
+        
+        if opc == '2':   
+            find_all_class(html)
+            link = chosse_class(html)        
+            print(link)
+        
+        if opc == '3':
+            break
+            
+            
+
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
