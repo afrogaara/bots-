c9ac3fdc82afe5b1501457eda0fcd71a6d506824
Roberto César <robertocoliver@protonmail.com>
2022-05-16 10:41:56 -0300

    crowler.py
    Crowler que encontra links, emails e telefones em um site na web.

--- crowler.py
+++ crowler.py
@@ -1,5 +1,13 @@
 import requests 
 from bs4 import BeautifulSoup # manipular html 
+import re
+CORREIO = list() 
+
+#crowler = set()
+def menu():
+    print("1) encontrar emails e telefones")
+    print("2) encontrar links")
+
 
 def cabecalho():
     header = dict()
@@ -65,24 +73,63 @@ def class_site(parsing):
         pass
     return lista 
     
-def encontrar_links():
-    pass
-def encontrar_telefones():
-    pass
-def econtrar_emails():
-    pass
 
+def find_emails(soup):
+    
+    try:
+        emails = re.findall(r"\w[\w\. ]+\w@\w[\w\ .]+\w", soup)
+         
+    except:
+        print("Não foi encontrado nenhum email")
+    return emails
+    "\w[\w\w.]+\w@\w[\w\.]+\w"
+    
+
+
+def fone(soup):
+    
+    try:
+           telefone = re.findall(r"(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})", soup) 
+    
+    except:
+        print("Não foi encontrado nenhum telefone")
+    return telefone
+    "(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})"
+    
+
+def saida(emails, telefone):
+    for email in emails:
+        if email not in CORREIO:
+            print(email)
+            CORREIO.append(email)
+    for cel in telefone:
+        for numero in cel:
+            print(numero, end=" ")
+        print(end="\n")
 url = input("URL: ")
+menu()
 
-crowler = set()
-
+opc = int(input("Digite a opção escolhida: "))
 if __name__== "__main__":
     header = cabecalho()
     soup = buscar_site(url, header)
     if soup:
-        parsing = parsing_site(soup)
-        link = links_site(parsing)
-
+        if opc == 1:
+            emails = find_emails(soup)
+            telefone = fone(soup)
+            saida(emails, telefone)
+        if opc == 2:
+            parsing = parsing_site(soup)    
+            link = links_site(parsing)
+            
+        
+        
         #lista = class_site(parsing)
         #encontrar_links(lista, parsing)
+
+
+
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# https://regex101.com/
+#https://docs.python.org/pt-br/dev/library/re.html
+#\w[\w\w.]+\w@\w[\w\.]+\w
